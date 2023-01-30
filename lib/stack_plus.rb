d7e1eb036165389776c7a2f3631e7732a091c242
edgar ludena <edgar.ludena90@gmail.com>
2023-01-30 08:06:42 -0500

    commit tree fcad4c4b35f1d59da30816d9c247b1de90076753

--- lib/stack_plus.rb
+++ lib/stack_plus.rb
@@ -1,17 +1,31 @@
 class StackPlus
+  attr_reader :data
+
   def initialize
-    # your code here
+    # Use an array as the underlying data structure
+    @data = []
   end
 
   def push(value)
-    # your code here
+    # Push a value onto the array
+    data.push(value)
   end
 
   def pop
-    # your code here
+    # Edge case: return -1 if the array is empty
+    return -1 unless data.size > 0
+
+    # Otherwise, just pop the top element from the array
+    data.pop
   end
 
   def increment(n)
-    # your code here
+    # We need to iterate over n elements from the array,
+    # but if there are fewer than n elements in the array,
+    # we can just iterate over the length of the array
+    [n, data.size].min.times do |i|
+      # increment the value of each element by 1
+      data[i] += 1
+    end
   end
 end
